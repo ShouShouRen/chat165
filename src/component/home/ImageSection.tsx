7a1f7092254b2696913a2303d5adc874fab096e4
Peter Chen <peterchen2017125@gmail.com>
2025-04-09 10:27:40 +0800

    feat: add image section

---
+++ src/component/home/ImageSection.tsx
@@ -0,0 +1,35 @@
+import Marquee from "react-fast-marquee";
+
+const images = [
+  { src: "https://picsum.photos/id/11/400/300", alt: "Card 1" },
+  { src: "https://picsum.photos/id/22/400/300", alt: "Card 2" },
+  { src: "https://picsum.photos/id/32/400/300", alt: "Card 3" },
+  { src: "https://picsum.photos/id/42/400/300", alt: "Card 4" },
+  { src: "https://picsum.photos/id/52/400/300", alt: "Card 5" },
+  { src: "https://picsum.photos/id/62/400/300", alt: "Card 6" },
+  { src: "https://picsum.photos/id/72/400/300", alt: "Card 7" },
+];
+
+const ImageCarousel = () => {
+  return (
+    <section className="pb-5 mb-8 h-96 flex items-center justify-center">
+      <Marquee pauseOnHover gradient={false} speed={50}>
+        <div className="flex gap-8">
+          {images.map((img, index) => (
+            <div
+              key={index}
+              className="min-w-[200px] h-[240px] overflow-y-hidden rounded-lg shadow-md"
+            >
+              <img
+                src={img.src}
+                alt={img.alt}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </Marquee>
+    </section>
+  );
+};
+export default ImageCarousel;
